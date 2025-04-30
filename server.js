f3a8f646c73e3ab575defc13e1b225209e43f602
neozvbambah <neozvbambah@gmail.com>
2025-04-30 16:08:55 +0530

    vineet

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'web.html'));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});

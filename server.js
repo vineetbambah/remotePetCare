792a6225ab78c1d70c790a225329da671dd96e8c
neozvbambah <neozvbambah@gmail.com>
2025-05-04 20:33:44 +0530

    bambah

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'web.html'));
 });
 
+app.get('/food', (req, res) => {
+    
+})
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
